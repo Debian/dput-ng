08c4ca9a985f7028ced8dc12438d909e429d5c89
Arno Töll <arno@debian.org>
2012-10-04 22:24:43 +0200

    Push some mental notes regarding changes file sanity checks

---
+++ dput/checkers/changes.py
@@ -0,0 +1,11 @@
+# for now, this is a placeholder
+#
+# backport/implement here:
+#
+# check whether -sa was missing when building the changes files
+# def check_source_needed(changes)
+#
+# check Changes.Distribution == debian/changelog
+# check_distribution(changes):
+#
+# [22:18] <daemonkeeper> Oh, I know another feature I want. for -2 upload, I want # to check whether the orig checksum matches the -1 upload.
