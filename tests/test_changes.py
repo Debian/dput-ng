cfbc026a5c3f827b44568f74a81f83c6426b7c30
Paul Tagliamonte <tag@pault.ag>
2012-10-03 20:39:23 -0400

    adding s'more tests

--- tests/test_changes.py
+++ tests/test_changes.py
@@ -6,6 +6,18 @@ from dput.exceptions import ChangesFileException
 chfd = 'tests/resources/changes/test.changes'
 test_changes = open(chfd, 'r').read()
 
+ref_obj = {
+    "Format": "1.8",
+    "Date": "Wed, 30 May 2012 22:17:42 -0400",
+    "Source": "srcpkg",
+    "Binary": "binpkg",
+    "Architecture": "source",
+    "Version": "1.3.2-1",
+    "Distribution": "unstable",
+    "Urgency": "low",
+    "Maintainer": "Paul Tagliamonte <paultag@debian.org>",
+    "Changed-By": "Paul Tagliamonte <paultag@ubuntu.com>"
+}
 
 def test_nonsense_fails():
     try:
@@ -32,7 +44,15 @@ def test_empty_changes():
 
 def test_parse_basics_string():
     ch = Changes(string=test_changes)
+    assert ch.get_filename() == None
+    for key in ref_obj:
+        assert ch[key] == ref_obj[key]
 
 
 def test_parse_basics_file():
     ch = Changes(filename=chfd)
+    assert ch.get_filename() == 'test.changes'
+    for key in ref_obj:
+        assert ch[key] == ref_obj[key]
+    assert ch.get_changes_file() == 'test.changes'
+    assert ch.get_files() == ['fileone', 'filetwo', 'filethree']
