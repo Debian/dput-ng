deb128ece7d12d66d362a19a8e70e3002dfdc912
Paul Tagliamonte <tag@pault.ag>
2012-10-03 21:33:23 -0400

    Migrating --> it's own file

---
+++ dput/checkers/lintian.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2012 dput authors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+import subprocess
+
+
+def process(output):
+    ret = []
+    for line in output.splitlines():
+        flag, line = line.split(":", 1)
+        flag = flag.strip()
+        if flag == "N":
+            continue
+        line = line.strip()
+        component, line = line.split(":", 1)
+        component = component.strip()
+        line = line.strip()
+        payload = line.split(" ", 1)
+
+        if len(payload) > 1:
+            tag, info = payload
+        else:
+            tag = payload[0]
+            info = ""
+
+        tag = tag.strip()
+        info = info.strip()
+        ret.append({
+            "severity": flag,
+            "component": component,
+            "tag": tag,
+            "info": info
+        })
+    return ret
+
+
+def lint(path, pedantic=False, info=False, experimental=False):
+    args = ["lintian", "--show-overrides"]
+
+    if pedantic:
+        args.append("--pedantic")
+    if info:
+        args.append("-I")
+    if experimental:
+        args.append("-E")
+
+    args.append(path)
+    big_error = False
+
+    try:
+        output = subprocess.check_output(args)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+        big_error = True
+
+    return process(output)
+
+
+def lintian():
+    pass
