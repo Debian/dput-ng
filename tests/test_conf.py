e5de27d9172ea6e3af7d95c9111da246d51f66ae
Paul Tagliamonte <tag@pault.ag>
2012-10-16 21:05:53 -0400

    Enable the config test.

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -6,17 +6,17 @@ import dput.profile
 
 dput.core._enable_debugging(2)
 
-dput.core.DPUT_CONFIG_LOCATIONS = [
-    "tests/resources/defklob/cf1/dput.cf",
-    "tests/resources/defklob/cf2/dput.cf"
-]
+dput.core.DPUT_CONFIG_LOCATIONS = {
+    "tests/resources/defklob/cf1/dput.cf": 15,
+    "tests/resources/defklob/cf2/dput.cf": 5
+}
 
-dput.core.CONFIG_LOCATIONS = [
-    "tests/resources/defklob/js1",
-    "tests/resources/defklob/js2"
-]
+dput.core.CONFIG_LOCATIONS = {
+    "tests/resources/defklob/js1": 20,
+    "tests/resources/defklob/js2": 10
+}
 
-#def test_default_klob():
-#    profile = dput.profile.load_profile("test")
-#    assert profile['name'] == 'test'
-#    assert profile['key1'] == 'foo'
+def test_default_klob():
+    profile = dput.profile.load_profile("test")
+    assert profile['name'] == 'test'
+    assert profile['key1'] == 'foo'
