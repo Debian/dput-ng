6f178e982c2fcf9ae085dde5e23e8281bcab3a72
Paul Tagliamonte <paultag@debian.org>
2013-01-12 17:49:49 -0500

    OK, back to 60% coverage.

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,5 +1,6 @@
 from dput.util import run_command
 from dput import upload
+from dput.exceptions import UploadException
 import dput.core
 import os.path
 import os
@@ -13,6 +14,7 @@ def _build_fnord():
     popdir = os.path.abspath(os.getcwd())
     os.chdir("tests/fake_package/fake-package-1.0")
     stdout, stederr, ret = run_command("dpkg-buildpackage -us -uc -S")
+    os.unlink("../fnord_1.0_source.test.upload")
     os.chdir(popdir)
     return os.path.abspath("tests/fake_package/fnord_1.0_source.changes")
 
@@ -20,3 +22,13 @@ def _build_fnord():
 def test_upload():
     path = _build_fnord()
     upload(path, 'test')
+
+
+def test_double_upload():
+    path = _build_fnord()
+    upload(path, 'test')
+    try:
+        upload(path, 'test')
+        assert True is False
+    except UploadException:
+        pass
