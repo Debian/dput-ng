00218b72326129e594d3891de028f075b25a8df3
Paul Tagliamonte <paultag@debian.org>
2013-01-12 17:15:23 -0500

    Adding in some new tests, trying to restore sanity.

---
+++ tests/test_configs.py
@@ -0,0 +1,35 @@
+from dput.util import validate_object, load_config
+from dput.exceptions import InvalidConfigError
+import dput.core
+import os
+
+
+dput.core.CONFIG_LOCATIONS = {
+    os.path.abspath("./tests/dputng"): 0
+}  # Isolate.
+
+
+def test_config_load():
+    obj = load_config('profiles', 'test_profile')
+    comp = {
+        "incoming": "/dev/null",
+        "method": "local",
+        "meta": "ubuntu"
+    }
+    for key in comp:
+        print key, obj[key], comp[key]
+        assert obj[key] == comp[key]
+
+
+def test_config_validate():
+    obj = load_config('profiles', 'test_profile')
+    validate_object('config', obj, "profiles/test_profile")
+
+
+def test_config_invalidate():
+    obj = load_config('profiles', 'test_profile_bad')
+    try:
+        validate_object('config', obj, "profiles/test_profile_bad")
+        assert True is False
+    except InvalidConfigError as e:
+        assert e.config_name == "profiles/test_profile_bad"
