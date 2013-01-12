06d46955c0f06d4b8e0a8c0409e8f77abfff90d0
Paul Tagliamonte <paultag@debian.org>
2013-01-12 17:46:29 -0500

    Adding in s'more bits

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,5 +1,22 @@
+from dput.util import run_command
 from dput import upload
+import dput.core
+import os.path
+import os
+
+dput.core.CONFIG_LOCATIONS = {
+    os.path.abspath("./tests/dputng"): 0
+}  # Isolate.
+
+
+def _build_fnord():
+    popdir = os.path.abspath(os.getcwd())
+    os.chdir("tests/fake_package/fake-package-1.0")
+    stdout, stederr, ret = run_command("dpkg-buildpackage -us -uc -S")
+    os.chdir(popdir)
+    return os.path.abspath("tests/fake_package/fnord_1.0_source.changes")
 
 
 def test_upload():
-    pass
+    path = _build_fnord()
+    upload(path, 'test')
