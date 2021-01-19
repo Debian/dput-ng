d9d2af1f6d31a1ed653f582e10072cc8db308afc
Stephan Sürken <absurd@debian.org>
2021-01-19 17:59:59 +0100

    uploaders/ftps.py (FtpsUploader): Add class docs.

--- dput/uploaders/ftps.py
+++ dput/uploaders/ftps.py
@@ -6,5 +6,10 @@ FTPS Uploader implementation.
 import dput.uploaders.ftp
 
 class FtpsUploader(dput.uploaders.ftp.FtpUploader):
+    """
+    Provides an interface to upload files through FTPS (RFC 4217), otherwise
+    just the same as the ftp uploader.
+    """
+
     def initialize(self, **kwargs):
         super().initialize(**kwargs, use_tls=True)
