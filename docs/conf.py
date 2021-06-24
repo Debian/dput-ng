65ba826198bec381ac27ca9d31bfd9d51b0dec96
李成刚 <lichenggang@uniontech.com>
2021-06-24 11:42:55 +0800

    fix: Could not import extension sphinx.ext.pngmath

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 extensions = [
     'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx',
-    'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath',
+    'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig'
 ]
 
